6cc9ee38912468003d6e37a30efd39313557700b
Shachar Yaron <>
2021-02-04 22:51:40 +0200

    add URL and query params to request

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,37 @@
 const express = require('express');
 const app = express();
 const axios = require("axios");
+const router = express.Router();
 
-const port = 3000;
-
+const port = process.env.PORT || 8080;
 const REDDIT_URL = "https://www.reddit.com";
 
-app.get('/', async (req, res) => {
-    let redditResponse = await getTopArticles("wallstreetbets", 2);
+router.get('/:subreddit', async (req, res) => {
+    const postsLimit = req.query.limit || 10;
+    let redditResponse = await getTopArticles(req.params.subreddit, postsLimit);
     console.log(redditResponse);
     res.send(redditResponse);
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+router.get('/', async (req, res) => {
+    res.send("server is alive");
 })
 
-let getTopArticles = async (subreddit, limit = 10) => {
+app.use('/api', router);
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`)
+})
+
+const getTopArticles = async (subreddit, limit = 10) => {
     let redditResponse;
     try {
         redditResponse = await axios.get(
             `${REDDIT_URL}/r/${subreddit}/top/.json`,
             {
-                't': 'all',
-                'limit': limit,
+                params: {
+                    't': 'all',
+                    'limit': 1
+                }
             });
     } catch (error) {
         console.log(`Internal server error.`);
