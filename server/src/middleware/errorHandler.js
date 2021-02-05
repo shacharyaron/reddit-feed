42e2ad4010be00147a59f0206552438e0c98b3d5
Shachar Yaron <>
2021-02-05 19:33:57 +0200

    hide messages for all server errors (5xx)

--- server/src/middleware/errorHandler.js
+++ server/src/middleware/errorHandler.js
@@ -2,7 +2,7 @@ const SERVER_ERROR_MESSAGE = "Internal Server Error."
 
 const errorHandler = (error, request, response, next) => {
     const errorStatus = error.status || 500;
-    const errorMessage = errorStatus === 500 ? SERVER_ERROR_MESSAGE : error.message;
+    const errorMessage = errorStatus >= 500 ? SERVER_ERROR_MESSAGE : error.message;
 
     response.status(errorStatus);
     response.json({
