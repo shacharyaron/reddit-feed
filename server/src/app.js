687271c0912fa5adad819559d5cca674c8645cfd
Shachar Yaron <>
2021-02-05 13:53:18 +0200

    use plurals for route

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const port = process.env.PORT || 8080;
 
-const subredditRouter = require('./routes/subredditRouter');
+const subredditRouter = require('./routes/subredditsRouter');
 
 const app = express();
-app.use('/subreddit', subredditRouter);
+app.use(express.json());
+app.use('/v1/subreddits', subredditRouter);
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
