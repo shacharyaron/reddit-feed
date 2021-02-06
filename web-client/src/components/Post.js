7c1bbcd32bcedbff8bcd2b0e2f27e592f2e66874
Shachar Yaron <shacharyaron5@gmail.com>
2021-02-06 19:25:55 +0200

    make images visible in posts

--- web-client/src/components/Post.js
+++ web-client/src/components/Post.js
@@ -3,19 +3,33 @@ import './css/Post.css';
 
 const Post = props => {
 
+    const isImage = (url) => {
+        return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+    }
+
+    const isLinkToOriginalPost = (url) => {
+        return (url.startsWith("https://www.reddit.com/r/"));
+    }
+
     const convertTimestampToLocalDate = (timestamp) => {
         const epochTime = new Date(1970, 0, 1);
         epochTime.setSeconds(timestamp);
         return epochTime.toDateString();
     }
 
-    const formatNumberSiSymbol = (number) => {
+    const convertToSiFormat = (number) => {
         if (number < 1e3) return number;
         if (number >= 1e3 && number < 1e6) return +(number / 1e3).toFixed(1) + "k";
         if (number >= 1e6 && number < 1e9) return +(number / 1e6).toFixed(1) + "M";
         if (number >= 1e9 && number < 1e12) return +(number / 1e9).toFixed(1) + "B";
         if (number >= 1e12) return +(number / 1e12).toFixed(1) + "T";
     }
+
+    const Content = (props) => {
+        if (isImage(props.url)) return <img src={props.url} id={props.id} alt={props.title}/>;
+        if (!isLinkToOriginalPost(props.url)) return <a href={props.url}>{props.url}</a>;
+        return <div/>
+    };
 
     return (
         <article className='post-container'>
@@ -26,13 +40,12 @@ const Post = props => {
             </div>
 
             <div className='post-content'>
-                <h3>{props.text}</h3>
-                <h3>{props.url}</h3>
-                <h3>{props.thumbnail}</h3>
+                {props.text}
+                <Content url={props.url}/>
             </div>
 
             <div className='post-information'>
-                <h3>{formatNumberSiSymbol(props.upvotes)}</h3>
+                <h3 id='post-upvotes'>{convertToSiFormat(props.upvotes)}</h3>
                 <h3>{props.author}</h3>
             </div>
 
--- web-client/src/components/PostsList.js
+++ web-client/src/components/PostsList.js
@@ -50,6 +50,51 @@ const PostsList = props => {
             "upvotes": 67935,
             "url": "https://i.redd.it/yzkuw7vj3c631.png",
             "thumbnail": "https://b.thumbs.redditmedia.com/AAsHev79VWPv9w8KNZQWKamzu26XoygBM5IEAoBta9U.jpg"
+        }, {
+            "id": "d7sv82",
+            "title": "The entire first arc of the Pokemon anime summarized in one GIF",
+            "text": null,
+            "author": "niggamachine312",
+            "createdTimestamp": 1569170169,
+            "upvotes": 64843,
+            "url": "https://i.redd.it/gsctu1vza6o31.gif",
+            "thumbnail": "https://b.thumbs.redditmedia.com/Tu2GWuAjo31L69DCD390JrgBMf5YBxeVdr8LgznaouU.jpg"
+        }, {
+            "id": "cbuzi3",
+            "title": "High quality animations from a Chinese bootleg pokemon game",
+            "text": null,
+            "author": "MrGamer419",
+            "createdTimestamp": 1562846943,
+            "upvotes": 58724,
+            "url": "https://i.redd.it/yv2vqtyk0o931.gif",
+            "thumbnail": "https://b.thumbs.redditmedia.com/bMaylPxzuje8Mw66O8QrVyWPJnpKl51EC6Jf92Xr2Yk.jpg"
+        }, {
+            "id": "eepbz9",
+            "title": "Surprising my best friend who hasn’t gotten to play Pokémon since Sun and Moon with Shield and the means to play",
+            "text": null,
+            "author": "johnnycoolname",
+            "createdTimestamp": 1577129707,
+            "upvotes": 58173,
+            "url": "https://v.redd.it/4aq6a6njqf641",
+            "thumbnail": "https://b.thumbs.redditmedia.com/rvzOH0eTTLwdRJBjF1dnP-LvhpKOeixhfe0r7bJlXws.jpg"
+        }, {
+            "id": "c1buqz",
+            "title": "A comic I made about the National Dex cut",
+            "text": null,
+            "author": "FestusFlare",
+            "createdTimestamp": 1560702911,
+            "upvotes": 58068,
+            "url": "https://i.redd.it/ihx65yyaxq431.png",
+            "thumbnail": "https://a.thumbs.redditmedia.com/0E_1TI9AIkbsdO8ZRpeExIBzfRgysv1aUyHDg7YEOb4.jpg"
+        }, {
+            "id": "e95nrx",
+            "title": "*Sacrifice tactics intensifies* [OC]",
+            "text": null,
+            "author": "DarkFangz",
+            "createdTimestamp": 1576061892,
+            "upvotes": 56326,
+            "url": "https://i.redd.it/cmjoy4sljz341.jpg",
+            "thumbnail": "https://b.thumbs.redditmedia.com/y17egocd8HaAaXKY6Q_nTthfZ_aSLDbkjnkhzjmOZUE.jpg"
         }]
     }
 
